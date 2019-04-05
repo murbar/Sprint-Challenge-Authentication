e184e6ee5e49701758ccfeb8873aef65c58adba4
Joel Bartlett <joel@murbar.com>
2019-04-05 14:13:50 -0400

    Fetch jokes

--- client/src/Jokes.jsx
+++ client/src/Jokes.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from '@reach/router';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div``;
 
-const Jokes = ({ data, user = null }) => {
-  if (!user) return <Redirect to="/login" noThrow />;
+const Jokes = ({ auth }) => {
+  if (!auth.user) return <Redirect to="/login" noThrow />;
+
+  const [jokes, setJokes] = useState([]);
+
+  const getJokes = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:3300/api/jokes');
+      setJokes(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getJokes();
+  }, []);
 
   return (
     <StyledDiv>
-      <h1>jokes</h1>
+      <h2>Some jokes for you, {auth.user.username}</h2>
+      {jokes.map(j => (
+        <p key={j.id}>{j.joke}</p>
+      ))}
     </StyledDiv>
   );
 };
