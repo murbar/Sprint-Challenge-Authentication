56c169b33ba4bb2261f667c6b5707ec68583bf2f
Joel Bartlett <joel@murbar.com>
2019-04-05 14:13:45 -0400

    Add login

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { navigate, Redirect, Link } from '@reach/router';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div``;
 
-const Login = () => {
-  return <StyledDiv>Login</StyledDiv>;
+const Login = ({ auth }) => {
+  const [credentials, setCredentials] = useState({});
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setCredentials(values => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    await auth.login(credentials.username, credentials.password);
+  };
+
+  if (auth.user) return <Redirect to="/" noThrow />;
+
+  return (
+    <div>
+      <h2>Log in</h2>
+      <p>
+        New user? <Link to="/register">Create an account</Link>
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="username"
+          type="text"
+          value={credentials.username || ''}
+          onChange={handleChange}
+          placeholder="Username"
+          autoFocus
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password || ''}
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
