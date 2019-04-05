fe9f241ba1e7f24cab81bd32fa0d13eb9fec311e
Joel Bartlett <joel@murbar.com>
2019-04-05 15:05:46 -0400

    Remove unused wrapper

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
-import styled from 'styled-components';
-
-const StyledDiv = styled.div``;
-
 const Home = ({ auth }) => {
   if (auth.user)
     return (
-      <StyledDiv>
+      <div>
         <h2>
           Hello, <span className="username">{auth.user.username}</span>!
         </h2>
         <p>
           <Link to="/jokes">Click here</Link> for some awesome dad jokes.
         </p>
-      </StyledDiv>
+      </div>
     );
 
   return (
-    <StyledDiv>
+    <div>
       <h2>Hello!</h2>
       <p>
         <Link to="/login">Log in</Link> or <Link to="/register">register</Link> to see some dad
         jokes!
       </p>
-    </StyledDiv>
+    </div>
   );
 };
 
